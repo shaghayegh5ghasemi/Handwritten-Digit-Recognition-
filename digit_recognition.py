213dfbf98d2c8dae3473a413c1b8716855b9a885
shaghayegh5ghasemi <52372702+shaghayegh5ghasemi@users.noreply.github.com>
2021-04-15 22:15:44 +0430

    second step is completed

--- digit_recognition.py
+++ digit_recognition.py
@@ -31,26 +31,26 @@ for n in range(num_of_train_images):
 
 
 # Reading The Test Set
-test_images_file = open('t10k-images.idx3-ubyte', 'rb')
-test_images_file.seek(4)
-
-test_labels_file = open('t10k-labels.idx1-ubyte', 'rb')
-test_labels_file.seek(8)
-
-num_of_test_images = int.from_bytes(test_images_file.read(4), 'big')
-test_images_file.seek(16)
-
-test_set = []
-for n in range(num_of_test_images):
-    image = np.zeros((784, 1))
-    for i in range(784):
-        image[i] = int.from_bytes(test_images_file.read(1), 'big') / 256
-    
-    label_value = int.from_bytes(test_labels_file.read(1), 'big')
-    label = np.zeros((10, 1))
-    label[label_value, 0] = 1
-    
-    test_set.append((image, label))
+# test_images_file = open('t10k-images.idx3-ubyte', 'rb')
+# test_images_file.seek(4)
+#
+# test_labels_file = open('t10k-labels.idx1-ubyte', 'rb')
+# test_labels_file.seek(8)
+#
+# num_of_test_images = int.from_bytes(test_images_file.read(4), 'big')
+# test_images_file.seek(16)
+#
+# test_set = []
+# for n in range(num_of_test_images):
+#     image = np.zeros((784, 1))
+#     for i in range(784):
+#         image[i] = int.from_bytes(test_images_file.read(1), 'big') / 256
+#
+#     label_value = int.from_bytes(test_labels_file.read(1), 'big')
+#     label = np.zeros((10, 1))
+#     label[label_value, 0] = 1
+#
+#     test_set.append((image, label))
 
 
 # Plotting an image
@@ -63,14 +63,34 @@ for n in range(num_of_test_images):
 # plt.show()
 
 #Second Step of the Project
+
+#Activation Function = Sigmoid
 def sig(x):
  return 1/(1 + np.exp(-x))
 
+#Weights with normal random numbers and zero biases
+first_layer_weight = np.matrix(np.random.randn(16,784))
+first_layer_bias = np.zeros(16).reshape(16,1)
+second_layer_weight = np.matrix(np.random.randn(16,16))
+second_layer_bias = np.zeros(16).reshape(16,1)
+third_layer_weight = np.matrix(np.random.randn(10,16))
+third_layer_bias = np.zeros(10).reshape(10,1)
+
+#output of the network for 100 inputs
+correct_output = 0
+wrong_output = 0
+#Calculate output of the network for 100 inputs and check the accuracy
 for i in range(100):
-     first_layer_weight = np.random.rand(16, 784)
-     first_layer_bias = np.zeros((16,1))
-     second_layer_weight = np.random.rand(16, 16)
-     second_layer_bias = np.zeros((16, 1))
-     third_layer_weight = np.random.rand(10, 16)
-     third_layer_bias = np.zeros((10, 1))
+    nn_inputs = train_set[i][0]
+    first_Layer_output = sig(first_layer_weight * nn_inputs + first_layer_bias)
+    second_layer_output = sig(second_layer_weight * first_Layer_output + second_layer_bias)
+    nn_outputs = sig(third_layer_weight * second_layer_output + third_layer_bias)
+    max_output = np.where(nn_outputs == np.amax(nn_outputs))[0][0]
+    if max_output == np.where(train_set[i][1] == 1)[0][0]:
+        correct_output += 1
+    else:
+        wrong_output += 1
+
+print(correct_output/100)
+
 
